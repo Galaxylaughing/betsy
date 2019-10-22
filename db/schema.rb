bc64505f0ae6116c1befa3251ca9b661d5d73e68
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-22 16:01:32 -0700

    user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_22_225754) do
+ActiveRecord::Schema.define(version: 2019_10_22_230107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 2019_10_22_225754) do
     t.string "photo_url"
     t.integer "stock"
     t.boolean "available", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
