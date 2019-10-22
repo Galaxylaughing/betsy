2ce7e298e0b51f0befaf81c38e8991b314513546
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-22 16:26:11 -0700

    made relationship migrations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  has_many :order_items
+  has_many :reviews
+  belongs_to :user
+  has_and_belongs_to_many :categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_22_230229) do
+ActiveRecord::Schema.define(version: 2019_10_22_232526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,10 +21,21 @@ ActiveRecord::Schema.define(version: 2019_10_22_230229) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "categories_products", force: :cascade do |t|
+    t.bigint "category_id"
+    t.bigint "product_id"
+    t.index ["category_id"], name: "index_categories_products_on_category_id"
+    t.index ["product_id"], name: "index_categories_products_on_product_id"
+  end
+
   create_table "order_items", force: :cascade do |t|
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "product_id"
+    t.bigint "order_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -46,6 +57,8 @@ ActiveRecord::Schema.define(version: 2019_10_22_230229) do
     t.boolean "available", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -53,6 +66,8 @@ ActiveRecord::Schema.define(version: 2019_10_22_230229) do
     t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "product_id"
+    t.index ["product_id"], name: "index_reviews_on_product_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,4 +77,8 @@ ActiveRecord::Schema.define(version: 2019_10_22_230229) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
+  add_foreign_key "products", "users"
+  add_foreign_key "reviews", "products"
 end
