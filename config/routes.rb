93445da532fcd7c118b2cf3bdc8f549fa19dcca8
Sam <samcoll@Samanthas-MBP.localdomain>
2019-10-23 14:49:27 -0700

    added routes for product, users, orders, reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # RESTful routes:
+  root to:'homepages#index'
+  
+  resources :users
+  resources :products
+  resources :orders
+  resources :reviews  
 end
