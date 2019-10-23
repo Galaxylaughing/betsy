454238c04835662fced1729c8a7ecc91deafb2cf
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-23 14:20:17 -0700

    added preliminary oath testing stuff

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,44 @@
 require "test_helper"
 
 describe UsersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "auth_callback" do
+    it "logs in an existing user and redirects to the root route" do
+      user = users(:begonia)
+      
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+      
+      expect {
+        get auth_callback_path(:github)
+      }.wont_change "User.count"
+      
+      must_redirect_to root_path
+      session[:user_id].must_equal user.id
+    end
+    
+    it "creates an account for a new user and redirects to the root route" do
+      new_user = User.new(provider: "github", uid: 888444, username: "newbie", email: "test@example.com")
+      
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(new_user))
+      
+      expect {
+        get auth_callback_path(:github)
+      }.must_differ "User.count", 1
+      
+      must_redirect_to root_path
+      session[:user_id].must_equal User.last.id
+    end
+    
+    it "redirects to the login route if given invalid user data" do
+      new_user = User.new(provider: "github", uid: 888444, username: "newbie", email: "test@example.com")
+      
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(new_user))
+      
+      expect {
+        get auth_callback_path(:github)
+      }.wont_change "User.count"
+      
+      must_redirect_to root_path
+      expect(flash[:error]).wont_be_nil
+    end
+  end
 end
