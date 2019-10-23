b6adf677d579a4daa9eb4cb9df828cb37d440581
Sam <samcoll@Samanthas-MBP.localdomain>
2019-10-23 14:31:04 -0700

    added relationships tests to product model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -19,7 +19,7 @@ describe Product do
     end
     
     it "is invalid if name is not unique" do
-      @product.name = products(:orquid).name
+      @product.name = products(:orchid).name
       expect(@product.valid?).must_equal false
       expect(@product.errors.messages).must_include :name
     end
@@ -76,6 +76,39 @@ describe Product do
       @product.user_id = nil
       result = @product.valid?
       expect(result).must_equal false
+    end
+  end
+  
+  describe "relationships" do
+    before do
+      @user = User.create(username: "test user", email: "test email") 
+      @category1 = Category.create(name: "flower")
+      @category2 = Category.create(name: "specialty")
+      @product = Product.create(user_id: @user.id, name: "test product", description: "cool product", price: 1.9, photo_url: "url", stock: 3, categories: [@category1, @category2]) 
+      
+      @review1 = Review.create(product_id: @product.id, rating: 5, comment: "great product")
+      @review2 = Review.create(product_id: @product.id, rating: 2, comment: "bad product")
+      
+      @order = Order.create(email: "test email", address: "test address", name: "test user name", cc_num: 1234, zip: 98111)
+      
+      @order_item1 = OrderItem.create(product_id: @product.id, order_id: @order.id, quantity: 2)
+      @order_item2 = OrderItem.create(product_id: @product.id, order_id: @order.id, quantity: 4)
+    end
+    
+    it "can have many reviews" do
+      expect(@product.reviews.count).must_equal 2
+    end
+    
+    it "can have many order items" do
+      expect(@product.order_items.count).must_equal 2
+    end
+    
+    it "can have many categories" do
+      expect(@product.categories.count).must_equal 2
+    end
+    
+    it "belongs to a user" do
+      assert_not_nil(@product.user_id)
     end
   end
 end
