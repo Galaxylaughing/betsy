00b618971a5f2a63691f79f256a07e89bf1bc6e5
iamsammysam <51973947+iamsammysam@users.noreply.github.com>
2019-10-23 15:43:40 -0700

    Update config/routes.rb
    Co-Authored-By: Sabrina Lowney <46505147+Galaxylaughing@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # RESTful routes:
-  root to:'homepages#index'
+  root 'homepages#index'
   
   resources :users
   resources :products
