2cebc9517ab7c06d42521215de9f5b750bf84fc6
Sam <samcoll@Samanthas-MBP.localdomain>
2019-10-22 20:18:06 -0700

    added column name to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_22_232526) do
+ActiveRecord::Schema.define(version: 2019_10_23_031436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2019_10_22_232526) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "name"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
