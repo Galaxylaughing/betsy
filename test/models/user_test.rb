6a71e6596892c8ed772b5ec7ca7ca5d5eaa432f4
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-23 10:28:24 -0700

    added photo url to product in tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,12 +5,15 @@ describe User do
     let(:merchant) {
       User.create(username: "Begonia", email: "b_begonia@example.com")
     }
+    let(:photo_url) {
+      "https://example_photo.com"
+    }
     
     it "can have a single product" do
       merchant_id = merchant.id
       description = "a small fern"
       
-      small_fern = Product.create(description: description, price: 5.25, stock: 3, user_id: merchant_id)
+      small_fern = Product.create(description: description, price: 5.25, stock: 3, user_id: merchant_id, photo_url: photo_url)
       saved_product = Product.find_by(description: description)
       
       expect(merchant.products).must_include saved_product
@@ -21,8 +24,8 @@ describe User do
       fern_description = "a small fern"
       cactus_description = "a large cactus"
       
-      small_fern = Product.create(description: fern_description, price: 5.25, stock: 3, user_id: merchant_id)
-      large_cactus = Product.create(description: cactus_description, price: 5.25, stock: 3, user_id: merchant_id)
+      small_fern = Product.create(description: fern_description, price: 5.25, stock: 3, user_id: merchant_id, photo_url: photo_url)
+      large_cactus = Product.create(description: cactus_description, price: 5.25, stock: 3, user_id: merchant_id, photo_url: photo_url)
       
       saved_fern = Product.find_by(description: fern_description)
       saved_cactus = Product.find_by(description: cactus_description)
