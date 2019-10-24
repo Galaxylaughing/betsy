f6a18c83c8a2b20ebbb3279d0068be282363bb7f
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-24 08:34:56 -0700

    fixed order_items_controller tests

--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -3,13 +3,23 @@ require "test_helper"
 describe OrderItemsController do
   describe 'relations' do
     it 'an orderitem belongs to a product' do
-      order_item = product(:)
-      expect(order_item.product_id).must_equal products(:begonia).id
+      begonia = products(:begonia)
+      begonia_id = products(:begonia).id
+      order_id = orders(:myrta_order).id
+      
+      order_item = OrderItem.create(quantity: 1, product_id: begonia_id, order_id: order_id)
+      
+      expect(order_item.product_id).must_equal begonia_id
     end
     
     it 'an orderitem belongs to an order' do
-      vote = votes(:hal_book)
-      expect(vote.user_id).must_equal users(:hallie).id
+      begonia = products(:begonia)
+      begonia_id = products(:begonia).id
+      order_id = orders(:myrta_order).id
+      
+      order_item = OrderItem.create(quantity: 1, product_id: begonia_id, order_id: order_id)
+      
+      expect(order_item.order_id).must_equal order_id
     end
   end
-  
+end
