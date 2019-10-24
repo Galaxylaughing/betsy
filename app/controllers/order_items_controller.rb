d7d3ca4f8f2e683a506bed61d58aad7dba6d6cda
Hallie <idhallie@gmail.com>
2019-10-24 15:42:24 -0700

    Added flash messages to OrderItems #create.

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -4,7 +4,7 @@ class OrderItemsController < ApplicationController
   def create
     if session[:order_id].nil?
       order = Order.create(address: "x", name: "x", cc_num: "x", cvv_code: "x", zip: "x", email: "blank@blank.com")
-      session[:order_id] = order.order_id
+      session[:order_id] = order.id
     end
     
     order_items = {
@@ -13,7 +13,15 @@ class OrderItemsController < ApplicationController
       order_id = session[:order_id]
     }
     
-    OrderItem.create(order_items)
+    order_item = OrderItem.new(order_items)
+    
+    if order_item.save
+      flash[:success] = "Successfully added item to your cart."
+      redirect_to products_path
+    else
+      flash.now[:failure] = "Failure: Item could not be added to your cart."
+      render :new
+    end
   end
   
   private
--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 describe OrderItemsController do
-  
-  
+  describe "Create" do
+    let(:product) { products(:begonia) }
+    it 'creates a new order_item successfully with valid data, and redirects the user to the product page'
+    
+  end
 end
 
