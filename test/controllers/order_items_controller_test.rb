897abcf48826cb4fd40e8f049518fae27698566c
Hallie <idhallie@gmail.com>
2019-10-24 16:44:04 -0700

    Added framework for controller test.

--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -1,9 +1,14 @@
 require "test_helper"
 
 describe OrderItemsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "create" do
+    let(:product) { products(:begonia)}
+    let(:quantity) { 1 }
+    let(:order) {Order.create(address: "x", name: "x", cc_num: "x", cvv_code: "x", zip: "x", email: "blank@blank.com")}
+    
+    it 'creates a new order_item successfully with valid data while not logged in, and redirects the user to the products page' do
+      
+    end
+  end
   
-end
-
+  
