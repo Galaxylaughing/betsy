17367c9f0b946ed94df765f47fab97529cf78162
Hallie <idhallie@gmail.com>
2019-10-24 09:16:54 -0700

    fixed order_items_controller test file (deleted garbage).

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,11 +1,2 @@
 require "test_helper"
 
-describe CategoriesController do
-  let ()
-  
-  describe "index" do
-    it "responds with success when there is at least one item in a category" do
-      
-    end
-  end
-end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
-describe Category do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+describe 'Category' do
+  describe 'relations' do
+    let(:annual) { Category.create!(name: "annual") }
+    let(:flower) { Category.create!(name: "flower") }
+    let(:begonia) { products(:begonia) }
+    let(:orchid) { products(:orchid) }
+    
+    it 'can have many products' do
+      annual.products << begonia
+      annual.products << orchid
+      
+      expect(annual.products.count).must_equal 2
+    end
+    
+    it 'can belong to many products' do
+      annual.products << begonia
+      annual.products << orchid
+      
+      expect(begonia.category).must_equal annual
+      expect(orchid.category).must_equal annual
+    end
+  end
 end
