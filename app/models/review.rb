f7898d2f79273639c2da85e01f35c13a92c81661
Hallie <idhallie@gmail.com>
2019-10-23 22:42:11 -0700

    added relations test.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :product
   
-  validates :rating, presence: true, inclusion: 1..5
+  validates :rating, presence: true, numericality: {only_integer: true, inclusion: 1..5}
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 describe Review do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  before do
+    @product = products(:begonia)
+    @review = Review.create!(rating: 4, comment: "I love the pretty flowers!", product_id: @product.id)
+  end
+  
+  describe 'relations' do
+    it 'belongs to a product' do
+      expect(@review.product_id).must_equal @product.id
+    end
+  end
+  
+  describe 'validations' do
+    before do
+      before do
+        let(:product) { products(:begonia) }
+        let(:review) { Review.create!(rating: 4, comment: "I love the pretty flowers!", product_id: @product.id) }
+      end
+      
+      it 'is valid when there is a rating between 1 and 5 entered' do
+        expect(review.valid?).must_equal true
+        
+        review.rating = 1
+        expect(review.valid?).must_equal true
+        
+        review.rating = 5
+        expect(review.valid?).must_equal true
+      end
+      
+      it 'is not valid with a rating that is not between 1 and 5' do
+        
+      end
+    end
+  end
 end
