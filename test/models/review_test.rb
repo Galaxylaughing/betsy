e1cc916463376027b29f7ac79b7fcff73a0a3fa1
Hallie <idhallie@gmail.com>
2019-10-23 22:44:33 -0700

    added validations tests for valid numbers.

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -13,25 +13,22 @@ describe Review do
   end
   
   describe 'validations' do
-    before do
-      before do
-        let(:product) { products(:begonia) }
-        let(:review) { Review.create!(rating: 4, comment: "I love the pretty flowers!", product_id: @product.id) }
-      end
+    let(:product) { products(:begonia) }
+    let(:review) { Review.create!(rating: 4, comment: "I love the pretty flowers!", product_id: @product.id) }
+    
+    it 'is valid when there is a rating between 1 and 5 entered' do
+      expect(review.valid?).must_equal true
       
-      it 'is valid when there is a rating between 1 and 5 entered' do
-        expect(review.valid?).must_equal true
-        
-        review.rating = 1
-        expect(review.valid?).must_equal true
-        
-        review.rating = 5
-        expect(review.valid?).must_equal true
-      end
+      review.rating = 1
+      expect(review.valid?).must_equal true
       
-      it 'is not valid with a rating that is not between 1 and 5' do
-        
-      end
+      review.rating = 5
+      expect(review.valid?).must_equal true
+    end
+    
+    it 'is not valid with a rating that is not between 1 and 5' do
+      
     end
   end
 end
+
