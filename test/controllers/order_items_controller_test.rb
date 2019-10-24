89a97a2d932c8b04c35eeb6b248ec3a3ae5d197a
Hallie <idhallie@gmail.com>
2019-10-23 21:41:04 -0700

    Added relations tests for order_item

--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -1,15 +1,2 @@
 require "test_helper"
 
-describe OrderItemsController do
-  describe 'relations' do
-    it 'an orderitem belongs to a product' do
-      order_item = product(:)
-      expect(order_item.product_id).must_equal products(:begonia).id
-    end
-    
-    it 'an orderitem belongs to an order' do
-      vote = votes(:hal_book)
-      expect(vote.user_id).must_equal users(:hallie).id
-    end
-  end
-  
