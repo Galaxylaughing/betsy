5a6e2da4d24006d4592fbe541283cf433da75858
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-26 17:02:09 -0700

    orders controller tests

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -18,12 +18,48 @@ describe OrdersController do
       end
 
       it 'creates a new order successfully with valid data while not logged in, and redirects the user to the products page' do
+        order_hash = {
+          order: {
+            address: "Redmond", 
+            name: "Georgina", 
+            cc_num: "1111111111111111", 
+            cvv_code: "123", 
+            zip: "98004", 
+            email: "blank@gmail.com",
+            status: "pending"
+          }
+        }
+
         expect {
-          post orders_path, params: @order_hash[:order]
+          post orders_path, params: order_hash
         }.must_differ 'Order.count', 1
-        
         must_redirect_to root_path
       end
+    end
+  end
+
+  describe "Logged in users" do
+    before do
+      perform_login(users(:begonia))
+    end
+
+    it 'creates a new order successfully with valid data while logged in, and redirects the user to the products page' do
+      order_hash = {
+        order: {
+          address: "Redmond", 
+          name: "Georgina", 
+          cc_num: "1111111111111111", 
+          cvv_code: "123", 
+          zip: "98004", 
+          email: "blank@gmail.com",
+          status: "pending"
+        }
+      }
+
+      expect {
+        post orders_path, params: order_hash
+      }.must_differ 'Order.count', 1
+      must_redirect_to root_path
     end
   end
 end
