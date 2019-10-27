8531c32327dfb2726364f9b6702a27587b524675
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-27 13:46:05 -0700

    added view helper for masking credit card number

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
-module UsersHelper
+module UsersHelper  
+  def mask_cc(cc_num)
+    return "[unknown]" unless cc_num
+    # from https://stackoverflow.com/questions/1904573/string-operation-in-ruby-for-credit-card-number
+    masked = cc_num.gsub(/.(?=....)/, '*')
+    return masked
+  end
 end
