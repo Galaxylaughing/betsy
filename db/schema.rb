e61ac7bc893560b3c19852bcdd1fe338e5d49820
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-28 14:01:13 -0700

    migration to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_213520) do
+ActiveRecord::Schema.define(version: 2019_10_28_210014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_10_24_213520) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status", default: "pending"
+    t.string "exp_date"
   end
 
   create_table "products", force: :cascade do |t|
