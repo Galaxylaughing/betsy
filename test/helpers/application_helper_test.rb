52c2b5e0b7f1ac2719add9b89b429b1474efbdc7
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-28 16:43:49 -0700

    added render_price helper and implemented total and subtotal for orders in dashboard

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -12,4 +12,14 @@ describe ApplicationHelper do
       expect(result).must_equal "Oct 20, 2019"
     end
   end
+  
+  describe 'render_price' do
+    it "displays a price with two digits after the decimal" do
+      price = 57.599999999999994
+      
+      result = render_price(price)
+      
+      expect(result).must_equal "57.59"
+    end
+  end
 end
