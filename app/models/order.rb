59c9803883d89bb74519f9316538855e97e0718d
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-29 10:48:10 -0700

    take out the regex from order at exp_date

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
   
   validates :address, :name, :cc_num, :cvv_code, :zip, presence: true, on: :update
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, on: :update
-  validates :exp_date, presence: true, format: { (0[1-9]|10|11|12)/20[0-9]{2}$ }, on: :update
+  validates :exp_date, presence: true, on: :update
   
   def total
     total = 0
