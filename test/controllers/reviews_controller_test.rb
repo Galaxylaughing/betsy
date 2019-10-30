7bebcb7e2dec4d1dbd8df11133bc94e398243f68
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-30 13:03:35 -0700

    reviews added to product

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 describe ReviewsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe 'guest users' do
+    describe "index" do
+      it "gives back a susccesful response" do
+        get orders_path
+        must_respond_with :success
+      end
+      
+      it "can get the root path" do
+        get root_path
+        must_respond_with :success
+      end
+    end
 end
