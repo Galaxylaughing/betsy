0a9a3f9a0ae5019e78475cc97a207c793b6e3bd2
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-29 21:22:15 -0700

    reviews controllers

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,29 @@
 class ReviewsController < ApplicationController
+  before_action :find_order, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @review = Review.new
+  end
+  
+  def index
+    @reviews = Review.all
+  end
+  
+  def show ; end
+  
+  def edit ; end
+
+  def create
+    @review = Review.new(review_params) 
+    
+    if @review.save
+      flash[:success] = "Your review was successfully submited."
+      redirect_to root_path
+      return
+    else
+      flash[:failure] = "Your review couldn't be submited."
+      redirect_to root_path
+      return
+    end
+  end 
 end
